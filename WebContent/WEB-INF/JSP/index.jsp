3a9fa7853dd42842c516367670d511282a065647
Frederic <frederic.petteloot@telenet.be>
2017-03-24 13:03:20 +0100

    Pizza 11.8

---
+++ WebContent/WEB-INF/JSP/index.jsp
@@ -0,0 +1,25 @@
+<%@page contentType='text/html' pageEncoding='UTF-8' session='false'%>
+<%@taglib prefix='c' uri="http://java.sun.com/jstl/core"%>
+<!doctype html>
+<html lang='nl'>
+<head>
+	<c:import url='head.jsp'>
+		<c:param name='title' value='Pizza Luigi'/>
+	</c:import>
+</head>
+<body>
+	<c:import url='menu.jsp'/>
+	<h1>Pizza Luigi</h1>
+	<img src='images/pizza.jpg' alt='pizza' class='fullwidth'>
+	<h2>${begroeting}</h2>
+	<h2>De zaakvoerder</h2>
+	<dl>
+		<dt>Naam</dt><dd>${zaakvoerder.naam}</dd>
+		<dt>Aantal kinderen</dt><dd>${zaakvoerder.aantalKinderen}
+		<dt>Gehuwd</dt><dd>${zaakvoerder.gehuwd ? 'Ja' : 'Nee'}</dd>
+		<dt>Adres</dt>
+		<dd>${zaakvoerder.adres.straat} ${zaakvoerder.adres.huisnr}<br>
+			${zaakvoerder.adres.postcode} ${zaakvoerder.adres.gemeente}</dd>
+	</dl>
+</body>
+</html> 
---
+++ src/be/vdab/servlets/IndexServlet.java
@@ -0,0 +1,28 @@
+package be.vdab.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import be.vdab.entities.Adres;
+import be.vdab.entities.Begroeting;
+import be.vdab.entities.Persoon;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/index.htm")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String VIEW="/WEB-INF/JSP/index.jsp";
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true, 
+					new Adres("Grote Markt", "3", 9700, "Oudenaarde")));
+			request.setAttribute("begroeting", new Begroeting());
+			request.getRequestDispatcher(VIEW).forward(request, response);
+	}
+}
+		
