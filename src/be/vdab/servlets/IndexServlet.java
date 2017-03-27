6cd4e8c989d6899561624605a4bc107ad0b1aba5
Frederic <frederic.petteloot@telenet.be>
2017-03-27 09:33:50 +0200

    PizzaLuigi 14 MultiThreading

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,6 +1,8 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +18,11 @@ import javax.servlet.annotation.WebServlet;
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW="/WEB-INF/JSP/index.jsp";
+	private final AtomicInteger aantalKeerBekeken = new AtomicInteger();
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
+		request.setAttribute("aantalKeerBekeken",  aantalKeerBekeken.incrementAndGet());
 			request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true, 
 					new Adres("Grote Markt", "3", 9700, "Oudenaarde")));
 			request.setAttribute("begroeting", new Begroeting());
